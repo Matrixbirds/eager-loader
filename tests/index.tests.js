fc4e203238e959dd4f8fba92b4e1c4b4b51b1f36
matrixbirds <yooobuntu@163.com>
2017-11-03 16:11:16 +0800

    fix: nested object from submodule is not copied

--- tests/index.tests.js
+++ tests/index.tests.js
@@ -10,3 +10,9 @@ test ("subdir", t => {
     t.falsy(models.Subdir.AT)
     t.truthy(models.Subdir.AModel)
 })
+
+test ("subdir nested object", t => {
+    t.truthy(models.Subdir.BModel)
+    t.deepEqual(models.Subdir.BModel, {name: "1024", tag: "1024"})
+})
+
---
+++ tests/models/subdir/b.model.js
@@ -0,0 +1,8 @@
+"use strict"
+module.exports = () => {
+    let a = {
+        name: "1024",
+        tag: "1024"
+    }
+    return a
+}
